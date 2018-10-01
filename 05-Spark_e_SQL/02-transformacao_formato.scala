98bcaa02962f791f980f3fa91b75af3a9425ac8c
cris_weber <cris_weber@bitbucket.org>
2018-10-01 19:19:17 -0300

    alteracao caminho

--- 05-Spark_e_SQL/02-transformacao_formato.scala
+++ 05-Spark_e_SQL/02-transformacao_formato.scala
@@ -1,6 +1,11 @@
 val nyc_jan = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("s3a://nyc-tlc/trip data/yellow_tripdata_2017-01.csv")
-nyc_jan.summary().show()
+val df = nyc_jan.summary()
+
+df.show()
+
+df.explain()
 
 nyc_jan.printSchema()
 
-nyc_jan.summary.write.parquet( "s3a://aula-spark/class_yellow_tripdata_2017-01.parquet" )
+
+df.write.parquet( "class_yellow_tripdata_2017-01.parquet" )
